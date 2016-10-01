ae21a80ef2f9cc0ba8cc9dc8ef232bab54cedb37
Vasu Jain <vasu.jain@live.in>
2016-09-30 23:42:26 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Tippy
+iOS (Swift) Tip Calculator
+
+Submitted by: Vasu Jain
+
+Time spent: 2 hours spent in total
+
+## User Stories
+
+The following functionality is complete:
+
+ [*] Enter a bill amount, calculate a tip and a total.
+ [*] Allow the user to select the tip percentage.
+ [*] Add a settings page allowing the user to select a default tip percentage.
+ 
+## GIF walkthrough
+ 
+Here's a walkthrough of implemented user stories:
+
+<img src='https://github.com/vasujain/tipmaster/blob/master/tipmaster.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+
+ 
+GIF created with [LiceCap](http://www.cockos.com/licecap/).
+
+## License
+
+    Copyright [2016] [Vasu Jain]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
---
+++ tippy/ViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift//
+//
+//  Created by Jain, Vasu on 9/30/16.
+//  Copyright © 2016 Vasu. All rights reserved.
+//
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+   
+    @IBOutlet weak var tipControl: UISegmentedControl!
+  
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "bg.png")!)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func onTap(_ sender: AnyObject) {
+         view.endEditing(true)
+    }
+ 
+  
+    @IBAction func calculateTip(_ sender: AnyObject) {
+        let tipPercentages = [0.18, 0.2, 0.25]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        let defaults = UserDefaults.standard
+        let percentSetting = defaults.integer(forKey: "tip_percent")
+        
+        tipControl.selectedSegmentIndex = percentSetting
+        self.calculateTip(self)
+        super.viewWillAppear(animated)
+        print("view will appear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did appear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view will disappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("view did disappear")
+    }
+
+}
+
